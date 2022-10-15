5a415e34e75dd5cc01288923cf6f5ecc16c1b447
Muhammad Fahriansyah <fachryansyah123@gmail.com>
2022-10-15 19:02:28 +0700

    add build config

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,22 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
+import { resolve } from "node:path";
+import * as packageJson from "./package.json";
 
 export default ({ mode }) => {
   return defineConfig({
     plugins: [react()],
+    build: {
+      lib: {
+        entry: resolve("src", "components/index.jsx"),
+        name: "ReactMazerUI",
+        formats: ["es", "umd"],
+        fileName: (format) => `react-mazer-ui.${format}.js`,
+      },
+      rollupOptions: {
+        external: [...Object.keys(packageJson.peerDependencies)],
+      },
+    },
     define: {
       "process.env.NODE_ENV": `"${mode}"`,
     },
